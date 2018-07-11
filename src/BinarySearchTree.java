8dd51a4e519cb7b5adb95a12487f5d569155b43e
Divya Singh <singh.d@husky.neu.edu>
2018-07-11 15:46:36 -0400

    bst insertion

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,15 +1,19 @@
 
 public class BinarySearchTree {
+	
+	public static Node insert(Node root, int data) {
+		return insertData(root, data);
+	}
 
-	public static Node insert(Node root, int data){
+	public static Node insertData(Node root, int data){
 		if(root == null) {
 			return new Node(data);
 		} 
-		else if(data <= root.data) {
-			root.left = insert(root.left,data);
+		else if(data < root.data) {
+			root.left = insertData(root.left,data);
 		}
-		else {
-			root.right = insert(root.right, data);
+		else if(data > root.data){
+			root.right = insertData(root.right, data);
 		}
 		return root;
 	}
@@ -33,21 +37,72 @@ public class BinarySearchTree {
 		}
 		return maxElementinBST(root.right);
 	}
+	
+	/**
+	 * deleting a node can be done in 2 ways - 
+	 * 1) find node, find min in node.right subtree, copy the value in targeted node, delete duplicate node from right sub tree
+	 * 2) find node, find max in node.left subtree, copy the value in targeted node, delete duplicate node from left sub tree
+	 */
+	
+	public static Node delete(Node root, int data) {
+		if(root == null) {
+			return root;
+		} else if(data < root.data) {
+			return delete(root.left, data);
+		} else if(data > root.data) {
+			return delete(root.right, data);
+		} else {
+			if(root.left == null && root.right == null) {
+				root = null;
+			}
+			else if(root.left == null) {
+				root = root.right;
+			}
+			else if(root.right == null) {
+				root = root.left;
+			}
+			else {
+				int minData = minElementinBST(root.right);
+				root.data = minData;
+				root.right = delete(root.right,minData);
+			}
+		}
+		return root;
+	}
+	
+	public static void print(Node node) {
+		
+		if(node == null) {
+			return;
+		}
+		System.out.println(node.data);
+		print(node.left);
+		print(node.right);
+	}
 
 	public static void main(String[] args) {
 		Node node = null;
+		node = insert(node,4);
 		node = insert(node,1);
 		node = insert(node,2);
-		node = insert(node,3);
-		node = insert(node,4);
-		node = insert(node,5);
+		/*node = insert(node,3);
 		node = insert(node,6);
-		node = insert(node,7);
-		node = insert(node,8);
+		node = insert(node,5);
+		node = insert(node,7);*/
 		
 		System.out.println("Min: " + minElementinBST(node));
-		
+		 
 		System.out.println("Max: " + maxElementinBST(node));
+		
+		//print(node);
+		
+		LevelOrderTraversalBFS.levelOrderTraversalBFS(node);
+		System.out.println();
+		/*node = delete(node,5);
+		
+		LevelOrderTraversalBFS.levelOrderTraversalBFS(node);*/
+		
+		System.out.println("print root data: " + node.data);
 	}
 
 }
